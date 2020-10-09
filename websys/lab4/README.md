85dc9451b9347737155fd629f847193612f63dcc
librautoo <wangy63@rpi.edu>
2020-10-09 10:37:20 +0800

    update yuhao creativity

--- websys/lab4/README.md
+++ websys/lab4/README.md
@@ -1,1 +1,19 @@
 # Lab 4
+
+Team Members:
+Yuhao Wang
+Kristofer Kwan
+Sam Avis
+Yanshen Lin
+Phillip Chang
+
+Instructor:
+Dr.Callahan
+
+TA:
+Priyanshu Tripathi
+
+Creativity
+
+Yuhao Wang (wangy63)
+- What I make is that I create a speech button. This skill is inspired by a funny game video. Then I search Google for a lot of resources to learn how to create a normal speech button. I also have applied this similar method into our group project. Hope you can enjoy the AI voice, Lol.
--- websys/lab4/lab4.js
+++ websys/lab4/lab4.js
@@ -1,72 +1,111 @@
-function getOutlinePartA(element, numDashes, inBody){
-	if(inBody) {
-		element.setAttribute("onclick", "alertBox(this)");
-	}
-	if(element.tagName == "BODY") {
-		inBody = true;	
-	}
+function getOutlinePartA(element, numDashes, inBody) {
+    if (inBody) {
+        element.setAttribute("onclick", "alertBox(this)");
+    }
+    if (element.tagName == "BODY") {
+        inBody = true;
+    }
     let children = element.children;
     var result = "-".repeat(numDashes) + element.tagName + "\n";
-    for(var i = 0; i < children.length; i++){
-        result += getOutlinePartA(children[i], numDashes+1, inBody);
+    for (var i = 0; i < children.length; i++) {
+        result += getOutlinePartA(children[i], numDashes + 1, inBody);
         result = result.toLowerCase();
     }
     return result;
 }
 
-function getRootName(classname){
+function getRootName(classname) {
     var index = classname.search("root") + 5;
-    if(index == -1){
+    if (index == -1) {
         return ""
     }
-    return classname.substring(index,);
+    return classname.substring(index, );
 }
 
-function getOutlinePartB(classname, depth, name){
+function getOutlinePartB(classname, depth, name) {
     let children = document.getElementsByClassName(classname);
     var result = "-".repeat(depth) + name + "\n";
-    for(var i = 0; i < children.length; i++){
+    for (var i = 0; i < children.length; i++) {
         const rootName = getRootName(children[i].className)
-        result += getOutlinePartB(rootName, depth+1, children[i].tagName);
+        result += getOutlinePartB(rootName, depth + 1, children[i].tagName);
         result = result.toLowerCase();
     }
     return result;
 }
 
-function placeOutlinePartA(){
-   let outline = getOutlinePartA(document.getElementsByTagName("html")[0], 0, false);
-   document.querySelector("#info").innerHTML = outline;
+function placeOutlinePartA() {
+    let outline = getOutlinePartA(document.getElementsByTagName("html")[0], 0, false);
+    document.querySelector("#info").innerHTML = outline;
 }
 
-function placeOutlinePart1B(){
-    let outline = getOutlinePartB("html",0, "html");
+function placeOutlinePart1B() {
+    let outline = getOutlinePartB("html", 0, "html");
     document.querySelector("#part1b").innerHTML = outline;
- }
+}
 
 function alertBox(obj) {
-	alert(obj.tagName);	
+    alert(obj.tagName);
 }
 
 function lab4Part3Quote() {
-	var myNode = document.getElementsByClassName("root-div1")[0];
-	var cln = myNode.cloneNode(true);
-	cln.innerHTML = '"When the light turns green, you go. When the light turns red, you stop. But what do you do when the light turns blue with orange and lavender spots?" - Shel Silverstein' 
-	cln.setAttribute("id", "quote");
-	document.body.appendChild(cln);
+    var myNode = document.getElementsByClassName("root-div1")[0];
+    var cln = myNode.cloneNode(true);
+    cln.innerHTML = '"When the light turns green, you go. When the light turns red, you stop. But what do you do when the light turns blue with orange and lavender spots?" - Shel Silverstein'
+    cln.setAttribute("id", "quote");
+    document.body.appendChild(cln);
 }
 
 function lab4Part3Mouse() {
-	var nodes = document.getElementsByTagName("DIV");
-	for(var i = 0; i < nodes.length; i++) {
-		nodes[i].addEventListener("mouseover",function(){
-			this.style.background = "#f00";
-			this.style["margin-left"] = "10px";
-		});
-		nodes[i].addEventListener("mouseout",function(){
-			this.style.background = "#fff";
-			this.style["margin-left"] = "0px";
-		});
-	}
+    var nodes = document.getElementsByTagName("DIV");
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener("mouseover", function() {
+            this.style.background = "#f00";
+            this.style["margin-left"] = "10px";
+        });
+        nodes[i].addEventListener("mouseout", function() {
+            this.style.background = "#fff";
+            this.style["margin-left"] = "0px";
+        });
+    }
+}
+
+// Yuhao's speech button
+function textToSpeech() {
+    // get all voices that browser offers
+    var available_voices = window.speechSynthesis.getVoices();
+
+    // this will hold an english voice
+    var english_voice = '';
+
+    // find voice by language locale "en-US"
+    // if not then select the first voice
+    for (var i = 0; i < available_voices.length; i++) {
+        if (available_voices[i].lang === 'en-US') {
+            english_voice = available_voices[i];
+            break;
+        }
+    }
+    if (english_voice === '')
+        english_voice = available_voices[0];
+
+    // new SpeechSynthesisUtterance object
+    var utter = new SpeechSynthesisUtterance();
+    utter.rate = 1;
+    utter.pitch = 0.5;
+    utter.text = "Congratulations, you have found the speech button. This is a lab four given by dear Doctor Callahan. This lab is completed\
+    by Yuhao Wang, Kristofer Kwan, Sam Avis, Yanshen Lin, Phillip Chang. This lab is basically for testing our Javascript ability. Hope all audience can\
+    enjoy it.";
+    utter.voice = english_voice;
+
+    // event after text has been spoken
+    utter.onend = function() {
+        alert('Speech has finished');
+        return;
+    }
+
+    // speak
+    window.speechSynthesis.speak(utter);
+
 }
 
 placeOutlinePartA()
